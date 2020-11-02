95decc1af4dd59cd34b64a468b9c73a4df41d30a
Jo Dunham <scaffelpike@yahoo.com.au>
2020-11-02 20:39:35 +1100

    subscribe to communities - glitch

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :communities do
     resources :posts
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :subscriptions
 
   root to: "home#index"
   
